1d0dcef044f0f2dc47cc6b4c728fa94b32938f68
Hong Shik Branden Kim <kimploo@gmail.com>
2023-09-23 03:58:30 +0900

    load nvm manually

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,9 +1,14 @@
   #!/bin/bash
-  pm2 describe app > /dev/null
-  RUNNING=$?
+source /home/ec2-user/.bash_profile
 
-  if [ "${RUNNING}" -ne 0 ]; then
-    pm2 start pnpm --name app -- start
-  else
-    pm2 reload app
-  fi;
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 
+  
+pm2 describe app > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start pnpm --name app -- start
+else
+  pm2 reload app
+fi;
