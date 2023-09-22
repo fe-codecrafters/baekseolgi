c8a981107388cd1cb93cee508813eb01f72be31d
Hong Shik Branden Kim <kimploo@gmail.com>
2023-09-23 04:21:26 +0900

    add scripts to build first

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,6 @@
-  #!/bin/bash
+#!/bin/bash
+
+cd /var/frontend/ 
 source /home/ec2-user/.bash_profile
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -8,6 +10,7 @@ pm2 describe app > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
+  pnpm run build
   pm2 start pnpm --name app -- start
 else
   pm2 reload app
