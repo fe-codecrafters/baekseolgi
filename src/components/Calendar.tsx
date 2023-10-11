4a9b70742a9211f238d96206df563852ec462746
code-kay <kay8563@gmail.com>
2023-10-11 18:56:07 +0900

    feat: Calendar 오늘 날짜 표시

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -16,13 +16,14 @@ const today = {
 // 달력에서 요일 및 각 날짜를 표시하는 컴포넌트
 // props-day는 일, 월 혹은 12, 13 같이 요일이나 날짜 정보가 들어옴
 // props-type은 day로 들어온 정보가 무엇인지 표시. day라면 요일을, date라면 날짜를 표시
-const Day = ({day, type}) => {
+const Day = ({day, type, isThisMonth}) => {
+    const isToday = (isThisMonth && type === 'date' && day === today.date) || (isThisMonth && type === 'day' && days.indexOf(day) === today.day)
     return (
       <div className='flex flex-col items-center mx-1'>
-        <div className={`w-14 h-6 text-center${type === 'day' ? ' text-lg' : ''}${days.indexOf(day) === today.day || today.date === day? ' text-black font-semibold' : ' text-neutral-500'}`}>{day.id? day.id : day !== 0 ? day : null}</div>
+        <div className={`w-14 h-6 text-center${type === 'day' ? ' text-lg' : ''}${isToday? ' text-black font-semibold' : ' text-neutral-500'}`}>{day.id? day.id : day !== 0 ? day : null}</div>
         {type === 'date' ? (
           <div>
-            <div className={`w-14 h-14 rounded-lg bg-neutral-200`}></div>
+            <div className={`w-14 h-14 rounded-lg ${day.id? 'bg-yellow-200' :'bg-neutral-200'}`}></div>
           </div>
         
         ) : null}
@@ -31,10 +32,10 @@ const Day = ({day, type}) => {
   }
   
   // 달력에서 한 주를 표시하는 컴포넌트 - 7개의 Day 컴포넌트를 묶어주는 역할
-  const Week = ({week, type}) => {
+  const Week = ({week, type, isThisMonth}) => {
     return (
       <div className='weekly-row flex flex-row w-auto justify-start mt-1'>
-        {week.map(day => <Day day={day} type={type}/>)}
+        {week.map(day => <Day day={day} type={type} isThisMonth={isThisMonth}/>)}
       </div>
     )
   }
@@ -45,9 +46,9 @@ const Day = ({day, type}) => {
     const [selectedYear, setSelectedYear] = useState(today.year)
     const [selectedMonth, setSelectedMonth] = useState(today.month)
     const [monthData, setMonthData] = useState(filterMonthData(selectedMonth))
-
+    
     //리덕스 써야것는디...
-
+    const isThisMonth = selectedYear === today.year && selectedMonth === today.month
     function filterMonthData (selectedMonth) {
       return mockData.data.filter(data => data.month === selectedMonth)[0]
     }
@@ -124,8 +125,8 @@ const Day = ({day, type}) => {
           <div className="font-semibold">{`${selectedYear}년 ${selectedMonth}월`}</div>
           {type !== 'week' ? <button onClick={toNextMonth} >&nbsp;{`>`}</button> : null}
         </div>
-        <Week week={days} type={'day'} />
-        {weeks.map(week => <Week week={week} type={'date'}/>)}
+        <Week week={days} type={'day'} isThisMonth={isThisMonth} />
+        {weeks.map(week => <Week week={week} type={'date'} isThisMonth={isThisMonth}/>)}
       </div>
     )
   }
