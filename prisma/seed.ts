0fb88b00dd025b0a9a9e0a9b47d47a7d3172a55c
Hong Shik Branden Kim <kimploo@gmail.com>
2023-10-15 01:08:15 +0900

    feat: 시드 기능 초기 세팅

---
+++ prisma/seed.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function main() {
+  const Seolgis = await prisma.seolgi.createMany({
+    data: [
+      {
+        id: 1,
+        name: "SeolgiIcon-test-white",
+        color: "white",
+      },
+      {
+        id: 2,
+        name: "SeolgiIcon-test-blue",
+        color: "blue",
+      },
+      {
+        id: 3,
+        name: "SeolgiIcon-test-pink",
+        color: "pink",
+      },
+    ],
+  });
+
+  const testSeolgi = await prisma.user.upsert({
+    where: { email: "kimploo@gmail.com" },
+    update: {},
+    create: {
+      email: "kimploo@gmail.com",
+      name: "테스트설기",
+      Objective: {
+        create: {
+          title: "100설기 매일 들어오기",
+          description: "",
+        },
+      },
+    },
+  });
+
+  const attendances = await prisma.dailyAttendance.createMany({
+    data: [
+      {
+        title: "100설기 들어옴 - 1",
+        status: "PRESENT",
+        createdAt: new Date("2023-10-05T14:01:56"),
+        updatedAt: new Date("2023-10-05T14:01:56"),
+        seolgiId: 1,
+        objectiveId: testSeolgi.id,
+      },
+      {
+        title: "100설기 들어옴 - 2",
+        status: "PRESENT",
+        createdAt: new Date("2023-10-06T15:01:56"),
+        updatedAt: new Date("2023-10-07T14:01:56"),
+        seolgiId: 1,
+        objectiveId: testSeolgi.id,
+      },
+      {
+        title: "100설기 들어옴 - 3",
+        status: "PRESENT",
+        createdAt: new Date("2023-10-07T15:01:56"),
+        updatedAt: new Date("2023-10-08T14:01:56"),
+        seolgiId: 3,
+        objectiveId: testSeolgi.id,
+      },
+      {
+        title: "100설기 들어옴 - 4",
+        status: "PRESENT",
+        createdAt: new Date("2023-10-08T15:01:56"),
+        updatedAt: new Date("2023-10-09T14:01:56"),
+        seolgiId: 2,
+        objectiveId: testSeolgi.id,
+      },
+      {
+        title: "100설기 들어옴 - 5",
+        status: "PRESENT",
+        createdAt: new Date("2023-10-10T15:01:56"),
+        updatedAt: new Date("2023-10-11T14:01:56"),
+        seolgiId: 2,
+        objectiveId: testSeolgi.id,
+      },
+    ],
+  });
+  console.log({ Seolgis, testSeolgi, attendances });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
