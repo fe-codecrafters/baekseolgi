3c366386ab23c6c6fdd16397c4359d412f48bded
Hong Shik Branden Kim <kimploo@gmail.com>
2023-10-18 02:06:27 +0900

    feat: attendance CRUD

--- src/app/api/attendance/[id]/route.ts
+++ src/app/api/attendance/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { DailyAttendanceStatus, PrismaClient } from "@prisma/client";
 import { AttendanceWithSeolgi } from "@/types/response";
 const { dailyAttendance } = new PrismaClient();
 
@@ -66,14 +66,98 @@ export async function GET(
   return NextResponse.json({ data: attendance });
 }
 
-export async function POST() {
+export async function POST(request: NextRequest) {
+  // userId, objectiveId, seogiId, title, createdAt
+  // TODO: need type
+  const { userId, objectiveId, seolgiId, title, createdAt } =
+    (await request.json()) as {
+      userId: number;
+      objectiveId: number;
+      seolgiId: number;
+      title: string;
+      createdAt: string;
+    };
+
+  try {
+    await dailyAttendance.create({
+      data: {
+        userId,
+        objectiveId,
+        seolgiId,
+        status: "PRESENT",
+        title,
+        createdAt: new Date(createdAt),
+      },
+    });
+  } catch (e) {
+    console.error("POST /attendance/{id} Error: ", e);
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+
   return NextResponse.json({ data: "Health Check" });
 }
 
-export async function PUT() {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const id = Number(params.id);
+  if (!id) {
+    return NextResponse.json(
+      { error: "Bad request: check your path param" },
+      { status: 400 },
+    );
+  }
+
+  const { userId, objectiveId, seolgiId, title, createdAt, status } =
+    (await request.json()) as {
+      userId: number;
+      objectiveId: number;
+      seolgiId: number;
+      title: string;
+      status: DailyAttendanceStatus;
+      createdAt: string;
+    };
+
+  try {
+    await dailyAttendance.update({
+      where: { id },
+      data: {
+        userId,
+        objectiveId,
+        seolgiId,
+        status,
+        title,
+        createdAt: new Date(createdAt),
+      },
+    });
+  } catch (e) {
+    console.error("PUT /attendance/{id} Error: ", e);
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
   return NextResponse.json({ data: "Health Check" });
 }
 
-export async function DELETE() {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const id = Number(params.id);
+  if (!id) {
+    return NextResponse.json(
+      { error: "Bad request: check your path param" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    await dailyAttendance.delete({
+      where: { id },
+    });
+  } catch (e) {
+    console.error("DELETE /attendance/{id} Error: ", e);
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+
   return NextResponse.json({ data: "Health Check" });
 }
