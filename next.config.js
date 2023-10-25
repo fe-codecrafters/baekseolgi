82d4de3ac46d73a87077f2b32a1406e5c831cd16
Hong Shik Branden Kim <kimploo@gmail.com>
2023-10-26 03:57:58 +0900

    feat: 100설기 기술 블로그 초기 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
---
+++ src/app/dev-log/[...slug]/page.tsx
@@ -0,0 +1,69 @@
+import { notFound } from "next/navigation";
+import { allDevLogs } from "contentlayer/generated";
+
+import { Metadata } from "next";
+import { Mdx } from "@/components/dev-log/mdx-components";
+import { Header } from "@/components/Header";
+
+interface PostProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getPostFromParams(params: PostProps["params"]) {
+  console.log(params);
+  const slug = params?.slug?.join("/");
+  const post = allDevLogs.find((post) => {
+    return post.slugAsParams === decodeURIComponent(slug);
+  });
+
+  if (!post) {
+    null;
+  }
+
+  return post;
+}
+
+export async function generateMetadata({
+  params,
+}: PostProps): Promise<Metadata> {
+  const post = await getPostFromParams(params);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+  };
+}
+
+export async function generateStaticParams(): Promise<PostProps["params"][]> {
+  return allDevLogs.map((post) => ({
+    slug: post.slugAsParams.split("/"),
+  }));
+}
+
+export default async function PostPage({ params }: PostProps) {
+  const post = await getPostFromParams(params);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <div className="mx-auto flex h-screen w-full flex-col items-center">
+      <Header />
+      <article className="prose py-6">
+        <h1 className="mb-2">{post.title}</h1>
+        {post.description && (
+          <p className="mt-0 text-xl text-slate-700">{post.description}</p>
+        )}
+        <hr className="my-4" />
+        <Mdx code={post.body.code} />
+      </article>
+    </div>
+  );
+}
