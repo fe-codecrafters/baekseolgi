aab9588c811e9940b9c196aa7b4196a5e1976e52
Hong Shik Branden Kim <kimploo@gmail.com>
2023-11-04 00:47:38 +0900

    feat: Calendar 컴포넌트 타입 설정, 메인 페이지 RQ 적용 등

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,3 +13,22 @@ export type IconType = "desktop" | "mobile";
  * @type {string}
  */
 export type AttendanceInputType = "calendar" | "main";
+
+/**
+ * 한글 요일 표기 타입
+ * @type {string}
+ */
+export type DayNameType = "일" | "월" | "화" | "수" | "목" | "금" | "토";
+
+/**
+ * 요일과 일자
+ * day: 요일 (일요일:0 ~ 토요일:6)
+ * Date.prototype.getDay()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
+ *
+ * date: 일자
+ * Date.prototype.getDate()
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDate
+ *
+ */
+export type DayOrDate = "day" | "date";
