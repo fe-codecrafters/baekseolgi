4471862a8985d010935eda6b948d360a63aaa5dd
jeong-hae-rim <usemono33@gmail.com>
2023-11-08 01:27:42 +0900

    feat: 메인 페이지 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,18 @@ import { useState } from "react";
 import { Objective } from "@/components/Objective";
 import { Calendar } from "@/components/Calendar/Calendar";
 import SeolgiIcon from "@/icons/SeolgiIcon";
-
-import { AttendanceWithSeolgi } from "@/types/dto";
+import { useSelector } from "react-redux";
 import { useMonthlyAttendances } from "@/features/attendance/api/getAttendances";
 import { attendanceKeys } from "@/features/attendance/key";
 import LoadingIndicator from "@/components/LoadingIndicator";
+import { RootState } from "@/redux/store";
 
 export default function Home() {
-  //이 부분은 calendar.tsx 로직과 동일.
-  //따로 빼내서 customhook으로 만들거나 help 함수로 만들어도 괜찮을 것 같습니다.
-  const date = new Date();
-  const today = {
-    year: date.getFullYear(),
-    month: date.getMonth() + 1,
-    date: date.getDate(),
-    day: date.getDay(),
-  };
+  const dateState = useSelector((state: RootState) => state.date);
 
-  const [selectedYear, setSelectedYear] = useState(today.year);
-  const [selectedMonth, setSelectedMonth] = useState(today.month);
   const RQKey = attendanceKeys.month({
-    year: selectedYear,
-    month: selectedMonth,
+    year: dateState.year,
+    month: dateState.month,
     // TODO: userId, objectiveId도 데이터 확인할 수 있어야
     userId: 1,
     objectiveId: 1,
@@ -43,66 +33,14 @@ export default function Home() {
   const [seolgiSay, setSeolgiSay] = useState<SeolgiSizeKey>("objective");
   const [effect, setEffect] = useState(false);
 
-  const toPrevMonth = () => {
-    if (selectedMonth === 1) {
-      setSelectedYear(selectedYear - 1);
-      setSelectedMonth(12);
-    } else {
-      setSelectedMonth(selectedMonth - 1);
-    }
-  };
-
-  const toNextMonth = () => {
-    if (selectedMonth === 12) {
-      setSelectedMonth(1);
-      setSelectedYear(selectedYear + 1);
-    } else {
-      setSelectedMonth(selectedMonth + 1);
-    }
-  };
-
-  const monthStart = new Date(selectedYear, selectedMonth - 1, 1);
-  const monthEnd = new Date(selectedYear, selectedMonth, 0);
-
-  const dates: (number | AttendanceWithSeolgi)[] = [];
-
-  for (let i = 0; i < monthStart.getDay(); i++) {
-    dates.push(0);
-  }
-  for (let i = 1; i <= monthEnd.getDate(); i++) {
-    dates.push(i);
-  }
-  while (dates.length % 7 !== 0) {
-    dates.push(0);
-  }
-
-  if (data && data.attendance) {
-    data.attendance.forEach((date: AttendanceWithSeolgi) => {
-      const { id } = date;
-      if (dates.findIndex((el) => el === id)) {
-        dates[dates.findIndex((el) => el === id)] = date;
-      }
-    });
-  }
-
   if (isLoading) return <LoadingIndicator></LoadingIndicator>;
 
   return (
     <>
-      <CalendarHeader
-        toPrevMonth={toPrevMonth}
-        toNextMonth={toNextMonth}
-        selectedYear={selectedYear}
-        selectedMonth={selectedMonth}
-      />
+      <CalendarHeader />
       <Objective />
       {!isLoading && data && (
-        <Calendar
-          selectedYear={selectedYear}
-          selectedMonth={selectedMonth}
-          monthData={data?.attendance}
-          type={"week"}
-        />
+        <Calendar monthData={data.attendance} type={"week"} />
       )}
       <div className="flex h-[400px] flex-col items-center justify-between gap-[30px]">
         <div className="relative flex h-[120px] w-[400px] items-center justify-center rounded-2xl border-[1px] bg-primary-white p-4 text-primary-black shadow-lg">
