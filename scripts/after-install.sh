feb066e80f2e0e6eff321da3f86fa8bfd1f3215e
Hong Shik Branden Kim <kimploo@gmail.com>
2023-11-09 00:58:58 +0900

    revoke

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -4,3 +4,15 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 source /home/ec2-user/.bash_profile
 
 cd /var/frontend/
+  
+# Check if the node_modules directory exists
+if [ -d "node_modules/" ]; then
+  # If it exists, remove it
+  echo "node_modules/ directory exists. Removing it..."
+  rm -r node_modules/
+else
+  # If it doesn't exist, print a message
+  echo "node_modules/ directory does not exist. Nothing to do."
+fi 
+
+pnpm install
