35be6bb624c6cde16516823a3541ecb148ca1a6b
Hong Shik Branden Kim <kimploo@gmail.com>
2023-11-08 22:46:29 +0900

    feat: change script

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -6,8 +6,11 @@ source /home/ec2-user/.bash_profile
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 
   
-pm2 describe app > /dev/null
+pm2 describe 100seolgi-app > /dev/null
 RUNNING=$?
 
-pnpm run build
-pm2 start pnpm --name app -- start -p 8080
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start "pnpm run start" --name 100seolgi-app -- start -p 8080
+else
+  pm2 reload 100seolgi-app
+fi;
