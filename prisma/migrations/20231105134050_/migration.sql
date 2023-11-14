18264777d6ae134a6e5f177eb86838f2585eed70
Hong Shik Branden Kim <kimploo@gmail.com>
2023-11-15 02:02:05 +0900

    feat: reset db migration history

--- prisma/migrations/20231105134050_/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Objective" ALTER COLUMN "description" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Seolgi" ADD COLUMN     "bgFill" TEXT,
-ADD COLUMN     "blushFill" TEXT;
