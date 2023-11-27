857319cc672613706cd1fe640be418bb05634f67
Hong Shik Branden Kim <kimploo@gmail.com>
2023-11-27 22:14:55 +0900

    test: change deploy script

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-source /home/ec2-user/.bash_profile
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# source /home/ec2-user/.bash_profile
 
-cd /var/frontend/
+# cd /var/frontend/
 
 # Check if the node_modules directory exists
-if [ -d "node_modules/" ]; then
+# if [ -d "node_modules/" ]; then
   # If it exists, remove it
-  echo "node_modules/ directory exists. Removing it..."
-  rm -r node_modules/
-else
+  # echo "node_modules/ directory exists. Removing it..."
+  # rm -r node_modules/
+# else
   # If it doesn't exist, print a message
-  echo "node_modules/ directory does not exist. Nothing to do."
-fi 
+  # echo "node_modules/ directory does not exist. Nothing to do."
+# fi 
 
-pnpm install
-pnpm db-generate
+# pnpm install
+# pnpm db-generate
--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 
 
 cd /var/frontend/ 
 source /home/ec2-user/.bash_profile
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 
   
 pm2 describe 100seolgi-server > /dev/null
 RUNNING=$?
