0bde960080ad6dd58cc7697ebadd7492ee603758
Hong Shik Branden Kim <kimploo@gmail.com>
2023-12-09 23:48:41 +0900

    feat: dev-log를 인증에서 제외

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,12 @@ export default withAuth(
       // The default is to let the user continue if they have a valid JWT (basic authentication).
       // JWT verify가 1차로 진행된다. (그래서 토큰 유뮤만 확인하면 됨)
       authorized: ({ req, token }) => {
-        const isStatic = ["/_next/static", "/_next/image", "favicon.ico"].some(
-          (s) => req.nextUrl.pathname.startsWith(s),
-        );
+        const isStatic = [
+          "/_next/static",
+          "/_next/image",
+          "favicon.ico",
+          "/dev-log",
+        ].some((s) => req.nextUrl.pathname.startsWith(s));
         if (isStatic) return true;
         console.log("authorize callback token", token);
         return Boolean(token);
