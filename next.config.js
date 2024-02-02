fa391cfe948749b0f6620758b67d30ab0b946c3f
jeong-hae-rim <usemono33@gmail.com>
2024-02-02 15:05:05 +0900

    feat: pwa 세팅 2차

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withContentlayer } = require("next-contentlayer");
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -9,7 +22,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  swcMinify: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = withContentlayer(withPWA(nextConfig));
