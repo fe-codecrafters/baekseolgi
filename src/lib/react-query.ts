a9b73db78b42dd411ec177f4981dfb02fba4fc07
Hong Shik Branden Kim <kimploo@gmail.com>
2024-02-02 11:26:24 +0900

    feat: add console.log for query client in dev mode

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
+const DEV = process.env.NODE_ENV === "development";
 
-export const queryClient = new QueryClient();
+export const queryClient = (() => {
+  DEV && console.log("query client mounted");
+  return new QueryClient();
+})();
